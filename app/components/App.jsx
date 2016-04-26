015b62e49c570041a6c989ea29aafb00acd1fc15
HM-432 <hm432mc@gmail.com>
2016-04-26 10:15:56 -0300

    first react component

---
+++ app/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Note from './Note.jsx'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Note />
+    );
+  }
+}
+
